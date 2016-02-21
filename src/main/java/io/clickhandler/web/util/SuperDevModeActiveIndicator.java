c5ad8d5664025095f39b4261a40d31aa98e89ce2
Clay Molocznik <clay@workreef.com>
2016-02-20 17:51:20 -0800

    added superdevmode detection

---
+++ src/main/java/io/clickhandler/web/util/SuperDevModeActiveIndicator.java
@@ -0,0 +1,7 @@
+package io.clickhandler.web.util;
+
+public class SuperDevModeActiveIndicator extends SuperDevModeIndicator {
+    public boolean isSuperDevMode() {
+        return true;
+    }
+}
---
+++ src/main/java/io/clickhandler/web/util/SuperDevModeIndicator.java
@@ -0,0 +1,7 @@
+package io.clickhandler.web.util;
+
+public class SuperDevModeIndicator {
+    public boolean isSuperDevMode() {
+        return false;
+    }
+}
